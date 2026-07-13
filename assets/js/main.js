926c3cf0bf4546f782a9c8c1a0fec6484492e56b
Mishan Ghimire <mishanghimire2003@gmail.com>
2026-07-13 21:13:10 +0545

    initial project setup

---
+++ assets/js/main.js
@@ -0,0 +1,68 @@
+/* =====================================================
+   Resume section tabs and tab contents
+===================================================== */
+
+/* =====================================================
+   Service modal open/close function
+===================================================== */
+
+/* =====================================================
+   Portfolio modals, tabs and cards
+===================================================== */
+
+// Filter portfolio cards according to portfolio tabs.
+
+// Open/Close Portfolio modals.
+
+/* =====================================================
+   Testimonial Swiper
+===================================================== */
+
+/* =====================================================
+   Send/Receive emails from contact form - EmailJS
+===================================================== */
+
+/* =====================================================
+   Shrink the height of the header on scroll
+===================================================== */
+
+/* =====================================================
+   Bottom navigation menu
+===================================================== */
+
+// Each bottom navigation menu items active on page scroll.
+
+// Javascript to show bottom navigation menu on home(page load).
+
+// Javascript to show/hide bottom navigation menu on home(scroll).
+
+// Hide bottom navigation menu on click menu-hide-btn.
+
+// Show bottom navigation menu on click menu-show-btn.
+
+/* =====================================================
+   To-top-button with scroll indicator bar
+===================================================== */
+
+/* =====================================================
+   Customized cursor on mousemove
+===================================================== */
+
+// Cursor effects on hover website elements.
+
+/* =====================================================
+   Website dark/light theme
+===================================================== */
+
+// Change theme and save current theme on click the theme button.
+
+// Get saved theme icon and theme on document loaded.
+
+/* =====================================================
+   ScrollReveal JS animations
+===================================================== */
+
+// Common reveal options to create reveal animations.
+
+// Target elements and specify options to create reveal animations.
+      
